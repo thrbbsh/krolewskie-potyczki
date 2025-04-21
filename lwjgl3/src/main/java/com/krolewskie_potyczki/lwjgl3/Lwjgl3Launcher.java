2201b61352c6af0388d0ccbad93c41a367adb2c1
Artur Rubish <artur.rubish@student.uj.edu.pl>
2025-04-21 16:01:24 +0200

    fixed problems

--- lwjgl3/src/main/java/com/krolewskie_potyczki/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/com/krolewskie_potyczki/lwjgl3/Lwjgl3Launcher.java
@@ -1,8 +1,8 @@
-package com.лещkrolewskie_potyczki.lwjgl3;
+package com.krolewskie_potyczki.lwjgl3;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.лещkrolewskie_potyczki.Main;
+import com.krolewskie_potyczki.Main;
 
 /** Launches the desktop (LWJGL3) application. */
 public class Lwjgl3Launcher {
