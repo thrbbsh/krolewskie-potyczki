7a47161114ecbe476fdfdfd18f64689147963a45
Artur Rubish <artur.rubish@student.uj.edu.pl>
2025-04-26 17:27:19 +0200

    Added MenuScreen, MVC architecture, UIScreen.
    Удаление ненужного файла assets/skins/pic.png

---
+++ core/src/main/java/com/krolewskie_potyczki/controller/MenuController.java
@@ -0,0 +1,22 @@
+package com.krolewskie_potyczki.controller;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.krolewskie_potyczki.Main;
+import com.krolewskie_potyczki.screens.GameScreen;
+
+public class MenuController {
+    private final Main game;
+
+    public MenuController(Main game) {
+        this.game = game;
+    }
+
+    public void onPlayClicked() {
+        game.setScreen(new GameScreen(game));
+    }
+
+    public void onExitClicked() {
+        Gdx.app.exit();
+    }
+}
