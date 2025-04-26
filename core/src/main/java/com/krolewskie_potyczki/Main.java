ac3dff01fcab18e4f3d57d548e80d8c0c2467548
Artur Rubish <artur.rubish@student.uj.edu.pl>
2025-04-26 17:27:19 +0200

    Added MenuScreen, MVC architecture, UIScreen.

--- core/src/main/java/com/krolewskie_potyczki/Main.java
+++ core/src/main/java/com/krolewskie_potyczki/Main.java
@@ -1,11 +1,12 @@
 package com.krolewskie_potyczki;
 
 import com.badlogic.gdx.Game;
+import com.krolewskie_potyczki.screens.MenuScreen;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends Game {
     @Override
     public void create() {
-        setScreen(new FirstScreen());
+        setScreen(new MenuScreen(this));
     }
 }
