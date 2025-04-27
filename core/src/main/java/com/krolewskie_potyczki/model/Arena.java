644224cfb4ab605069ad0fa7d2413366912433c2
Artur Rubish <artur.rubish@student.uj.edu.pl>
2025-04-27 21:25:55 +0200

    Added elixir + basic logic + elixir counter on GameScreen, changed EntityType logic

--- core/src/main/java/com/krolewskie_potyczki/model/Arena.java
+++ core/src/main/java/com/krolewskie_potyczki/model/Arena.java
@@ -5,10 +5,23 @@ import java.util.List;
 
 public class Arena {
     List<Entity> activeEntities;
+    private float playerElixir = 0;
+    private final float maxElixir = 10;
+    private float elixirSpeed = 0.1F;
+
+    public float getPlayerElixir() {
+        return (float) Math.floor(playerElixir * 10) / 10F;
+    }
+
+    public float getMaxElixir() {
+        return maxElixir;
+    }
+
     public Arena() {
         activeEntities = new ArrayList<>();
         addEntity(createEntity(EntityType.TOWER, true, 100, 600));
         addEntity(createEntity(EntityType.TOWER, false, 2300, 600));
+        playerElixir = 5;
     }
     public Entity createEntity(EntityType entityType, boolean isPlayersEntity, float x, float y) {
         switch (entityType) {
@@ -20,15 +33,25 @@ public class Arena {
                 throw new IllegalArgumentException("Wrong entity type: " + entityType);
         }
     }
-    public void update() {
+    public void update(float delta) {
+        updatePlayerElixir(delta);
         for (Entity entity : activeEntities)
             entity.updateCurrentTarget(activeEntities);
     }
+
+    private void updatePlayerElixir(float delta) {
+        playerElixir = Math.min(maxElixir, playerElixir + delta * elixirSpeed);
+    }
+
     public List<Entity> getActiveEntities() {
         return activeEntities;
     }
 
     public void addEntity(Entity entity) {
         activeEntities.add(entity);
+    }
+
+    public void subtractElixir(float elixirCost) {
+        playerElixir -= elixirCost;
     }
 }
--- core/src/main/java/com/krolewskie_potyczki/view/GameView.java
+++ core/src/main/java/com/krolewskie_potyczki/view/GameView.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
@@ -22,29 +23,42 @@ public class GameView implements Disposable {
     private Stage stage;
     private GameController controller;
     private Arena arena;
+    private Label currentElixir;
 
     public GameView(Arena arena, GameController controller) {
         this.arena = arena;
         arenaView = new ArenaView(arena);
         stage = new Stage();
         skin = new Skin(Gdx.files.internal("assets/skins/craftacular-ui.json"));
+
         TextButton spawnButton = new TextButton("Spawn square", skin);
         spawnButton.setPosition(10, 10);
         spawnButton.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent changeEvent, Actor actor) {
-                Entity e = controller.createEntity(EntityType.SQUARE, true,100, 100);
-                arenaView.addEntityView(new EntityView(e));
+                if (arena.getPlayerElixir() >= EntityType.SQUARE.getElixirCost()) {
+                    Entity e = controller.createEntity(EntityType.SQUARE, true, 100, 100);
+                    arenaView.addEntityView(new EntityView(e));
+                    arena.subtractElixir(EntityType.SQUARE.getElixirCost());
+                }
             }
         });
+
+        currentElixir = new Label("Current elixir: " + arena.getPlayerElixir() + "/" + arena.getMaxElixir(), skin);
+        currentElixir.setPosition(1900, 10);
+        currentElixir.setFontScale(1.5F);
+
+        stage.addActor(currentElixir);
         stage.addActor(spawnButton);
     }
 
     public void render(float delta) {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        arena.update();
+        currentElixir.setText("Current elixir: " + arena.getPlayerElixir() + "/" + arena.getMaxElixir());
+        arena.update(delta);
         arenaView.render(delta);
+        stage.getActors();
         stage.act(delta);
         stage.draw();
     }
