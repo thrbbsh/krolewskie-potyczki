b31755f0ddd5f5aea77bd577aba2a1e49a7e2f53
Roman Volochai <roman.volochai@student.tcs.uj.edu.pl>
2025-04-28 15:20:05 +0300

    Bugs were fixed.

--- core/src/main/java/com/krolewskie_potyczki/model/EntityType.java
+++ core/src/main/java/com/krolewskie_potyczki/model/EntityType.java
@@ -2,25 +2,25 @@ package com.krolewskie_potyczki.model;
 
 public enum EntityType {
     TOWER(0, 0, 1000, 100, 50, 0.5f, false),
-    SQUARE(3, 300, 300, 20, 100, 0.5f, false);
+    SQUARE(3, 300, 300, 20, 100, 0.5f, false),
     TRIANGLE(5, 100, 700, 20, 200, 1.5f, true);
 
     private final float elixirCost;
     private final float moveSpeed;
     private final float totalHP;
-    private final float attackRadius; 
+    private final float attackRadius;
     private final float damage;
     private final float attackInterval;
-    private final boolean ignoresUnits; 
+    private final boolean ignoresUnits;
 
-    EntityType(float elixirCost, float moveSpeed, float hp, float attackRadius, float damage, float attackInterval, boolean ignoresUnits) { 
+    EntityType(float elixirCost, float moveSpeed, float hp, float attackRadius, float damage, float attackInterval, boolean ignoresUnits) {
         this.elixirCost = elixirCost;
         this.moveSpeed = moveSpeed;
         this.totalHP = hp;
-        this.attackRadius = attackRadius; 
+        this.attackRadius = attackRadius;
         this.damage = damage;
-        this.attackInterval = attackInterval; 
-        this.ignoresUnits = ignoresUnits; 
+        this.attackInterval = attackInterval;
+        this.ignoresUnits = ignoresUnits;
     }
 
     public float getElixirCost() {
@@ -38,16 +38,16 @@ public enum EntityType {
     public float getAttackRadius() {
         return attackRadius;
     }
- 
+
     public float getDamage() {
         return damage;
     }
 
     public float getAttackInterval() {
-        return attackInterval; 
+        return attackInterval;
     }
-  
+
     public boolean doesIgnoreUnits() {
-        return ignoresUnits; 
+        return ignoresUnits;
     }
 }
