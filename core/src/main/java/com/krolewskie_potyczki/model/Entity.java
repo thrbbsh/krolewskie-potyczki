c73951d55cdfafa273c7e95245a0e16e84b3975c
Roman Volochai <roman.volochai@student.uj.edu.pl>
2025-04-28 15:04:02 +0300

    Added attack mechanic, changed logic of EntityView. Added unit mortality.

--- core/src/main/java/com/krolewskie_potyczki/model/Entity.java
+++ core/src/main/java/com/krolewskie_potyczki/model/Entity.java
@@ -3,10 +3,12 @@ package com.krolewskie_potyczki.model;
 import java.util.List;
 
 public abstract class Entity {
-    float HP, x, y, damage;
+    float HP, x, y;
     boolean isPlayersEntity;
     Entity currentTarget;
     EntityType type;
+
+    private float timeSinceLastAttack = 0f;
 
     Entity(EntityType type, boolean isPlayersEntity, float x, float y) {
         currentTarget = null;
@@ -14,6 +16,7 @@ public abstract class Entity {
         this.x = x;
         this.y = y;
         this.type = type;
+        HP = type.getTotalHP();
     }
 
     float distance(Entity target) {
@@ -27,7 +30,7 @@ public abstract class Entity {
         float dx = currentTarget.x - this.x;
         float dy = currentTarget.y - this.y;
         float dist = distance(currentTarget);
-        if (dist < type.getAttackRadius()) return;
+        if (dist <= type.getAttackRadius()) return;
         x += dx / dist * delta * type.getMoveSpeed();
         y += dy / dist * delta * type.getMoveSpeed();
     }
@@ -36,7 +39,21 @@ public abstract class Entity {
         return isPlayersEntity;
     }
 
-    void update(float change) {
+    public void update(float delta, List<Entity> activeEntities) {
+        updateCurrentTarget(activeEntities);
+        move(delta);
+
+        timeSinceLastAttack += delta;
+
+        if (currentTarget != null && distance(currentTarget) <= type.getAttackRadius() &&
+            timeSinceLastAttack >= type.getAttackInterval()) {
+            attack();
+            timeSinceLastAttack = 0f;
+        }
+    }
+
+
+    void updateHP(float change) {
         HP = Math.min(type.getTotalHP(), Math.max(0F, HP + change));
     }
 
@@ -45,7 +62,19 @@ public abstract class Entity {
     }
 
     void attack() {
+        currentTarget.receiveDamage(type.getDamage());
+    }
 
+    void receiveDamage(float amount) {
+        updateHP(-amount);
+        if (isDead()) {
+            onDeath();
+        }
+    }
+
+    void onDeath() {
+        System.out.println("I am dead.");
+        // TODO (animation, case when tower is "dead")
     }
 
     public float getX() {
@@ -55,12 +84,16 @@ public abstract class Entity {
         return y;
     }
 
+    public float getHP() {
+        return HP;
+    }
+
     public void updateCurrentTarget(List<Entity> activeEntities) {
         currentTarget = null;
         for (Entity e : activeEntities) {
             if (e.isPlayersEntity == this.isPlayersEntity) continue;
             if (currentTarget == null) currentTarget = e;
-            else if (distance(currentTarget) < distance(e)) currentTarget = e;
+            else if (distance(currentTarget) > distance(e)) currentTarget = e;
         }
     }
 }
