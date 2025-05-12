47955a606ded4517375f62677e53833781bf30dd
Roman Volochai <roman.volochai@student.uj.edu.pl>
2025-05-12 16:02:43 +0200

    Small bugs were fixed.

--- core/src/main/java/com/krolewskie_potyczki/view/CardView.java
+++ core/src/main/java/com/krolewskie_potyczki/view/CardView.java
@@ -13,13 +13,13 @@ import com.krolewskie_potyczki.model.Card;
 import com.krolewskie_potyczki.model.EntityType;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 
-public class CardView implements Disposable { 
+public class CardView implements Disposable {
     private final Card card;
     private final ImageButton imageButton;
     private final Texture elixirDropTexture;
     private final SpriteBatch batch;
     private final BitmapFont font;
-    private final float x, y; 
+    private final float x, y;
 
     public CardView(EntityType type, float x, float y, CardClickListener listener) {
         this.x = x;
@@ -62,4 +62,8 @@ public class CardView implements Disposable {
     }
     @Override
     public void dispose() {}
+
+    public Card getCard() {
+        return card;
+    }
 }
