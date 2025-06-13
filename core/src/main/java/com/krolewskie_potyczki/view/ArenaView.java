7bf1a5ed376d6ffa388507b536c2ea4af7e5ea0c
Roman Volochai <roman.volochai@student.uj.edu.pl>
2025-06-13 22:03:50 +0200

    Fixed wrong cursor coordinates detection inside ArenaView.

--- core/src/main/java/com/krolewskie_potyczki/view/ArenaView.java
+++ core/src/main/java/com/krolewskie_potyczki/view/ArenaView.java
@@ -6,7 +6,6 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.Disposable;
 import com.krolewskie_potyczki.model.config.GameConfig;
